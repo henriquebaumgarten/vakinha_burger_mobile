04773c9646a61af975a8bdd0f2baefeacaf03085
HENRIQUE SCHUNK BAUMGARTEN <henriquebaumgarten@gmail.com>
2023-05-15 14:37:58 -0300

    vakinha burger mobile

---
+++ lib/app/core/rest_client/rest_client.dart
@@ -0,0 +1,22 @@
+import 'package:get/get_connect.dart';
+
+class RestClient extends GetConnect {
+  final _backendBaseUrl = 'http://localhost:8080';
+
+  RestClient() {
+    httpClient.baseUrl = _backendBaseUrl;
+  }
+}
+
+class RestClientException implements Exception {
+  final int? code;
+  final String message;
+
+  RestClientException(
+    this.message, {
+    this.code,
+  });
+
+  @override
+  String toString() => 'RestClientException(code: $code, message: $message)';
+}
